c1e95a6c6f7512de26f8c77d7a0fac580c4d3512
Sofía Martinelli <sofia@lucci.ai>
2024-08-05 13:10:37 -0600

    initial commit

---
+++ api/server.js
@@ -0,0 +1,32 @@
+const fs = require("fs");
+const path = require("path");
+const db = JSON.parse(fs.readFileSync(path.join("db.json")));
+
+// Ver https://github.com/typicode/json-server#module
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router(db);
+const middlewares = jsonServer.defaults();
+
+// Usar middlewares predeterminados
+server.use(middlewares);
+
+// Reescribir rutas antes de usar el router
+server.use(
+  jsonServer.rewriter({
+    "/api/chats": "/chats",
+    "/api/chats/:id": "/chats/:id",
+  }),
+);
+
+// Usar el router
+server.use(router);
+
+// Usar el puerto proporcionado por el entorno (como Vercel) o 3000 por defecto
+const port = process.env.PORT || 3000;
+server.listen(port, () => {
+  console.log(`El servidor JSON está funcionando en el puerto ${port}`);
+});
+
+// Exporta la API del Servidor
+module.exports = server;
